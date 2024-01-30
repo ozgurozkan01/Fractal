a73b1c4d440d09ca8539f319a6de7fe6f65df55c
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-01-30 23:51:36 +0300

    add new fractal type class which is called mandelbulb

---
+++ src/Mandelbulb/Mandelbulb.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 30/01/2024.
+//
+
+#include "Mandelbulb.h"
---
+++ src/Mandelbulb/Mandelbulb.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 30/01/2024.
+//
+
+#ifndef FRACTAL_MANDELBULB_H
+#define FRACTAL_MANDELBULB_H
+
+
+class Mandelbulb {
+
+};
+
+
+#endif //FRACTAL_MANDELBULB_H
