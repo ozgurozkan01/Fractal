8cf951d074c75b434b182cd5f191391e220c1368
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-01 22:11:34 +0300

    add new fractal type which is called BrownianTreeSnowflake

---
+++ src/Fractal/BrownianTreeSnowflake/BrownianTreeSnowflake.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 01/02/2024.
+//
+
+#include "BrownianTreeSnowflake.h"
---
+++ src/Fractal/BrownianTreeSnowflake/BrownianTreeSnowflake.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 01/02/2024.
+//
+
+#ifndef FRACTAL_BROWNIANTREESNOWFLAKE_H
+#define FRACTAL_BROWNIANTREESNOWFLAKE_H
+
+
+class BrownianTreeSnowflake {
+
+};
+
+
+#endif //FRACTAL_BROWNIANTREESNOWFLAKE_H
