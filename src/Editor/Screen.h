cbabc4096c39fd1c2dc7b4000c23df828b81cbb9
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-01 22:08:09 +0300

    refactor code to provide more readability and improve OOP

--- src/Editor/Screen.h
+++ src/Editor/Screen.h
@@ -6,40 +6,29 @@
 #define FRACTAL_SCREEN_H
 
 #include "SDL.h"
-#include "../Mandelbrot/Mandelbrot.h"
-#include "../Snowflake/Snowflake.h"
+#include "../Fractal/FractalCreator.h"
 
 class Screen
 {
 private:
-    const int windowWidth = 1000;
-    const int windowHeight = 800;
+    const int windowWidth = 800;
+    const int windowHeight = 600;
     const char* windowName = "Fractal_Simulation";
 
     SDL_Window* window{};
     SDL_Renderer* renderer{};
     SDL_Texture* texture{};
-    uint32_t * windowPixels;
 
     bool isWindowOpen;
 
-    Mandelbrot* mandelbrot;
-    Snowflake* snowflake;
-
-    int* histogram;
-    int* pixelIterations;
-
+    FractalCreator fractalCreator;
+    int drawnFractalNumber;
 public:
-
-    Screen();
+    Screen(int drawnFractalNumber);
     ~Screen();
 
     void init();
     void processEvent();
-    void drawFractal(int n);
-    void setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue);
-    void drawMandelbrot();
-    void drawSnowflake();
 };
 
 #endif //FRACTAL_SCREEN_H
